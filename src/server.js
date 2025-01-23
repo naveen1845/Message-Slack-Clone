4cc3fa602939931ecfbd9093a8b1471b2f712064
NaveenPattihal <naveenpattihal08@gmail.com>
2025-01-23 14:48:36 +0530

    added cors for socket

--- src/server.js
+++ src/server.js
@@ -12,7 +12,11 @@ import apiRouter from './routes/apiRouter.js';
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: '*'
+  }
+});
 
 app.use(cors());
 app.use(express.json());
